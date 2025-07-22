0b247477262e2cb3751697ff94de59f934546726
chrissy45 <117347389+Christabel-Quaye@users.noreply.github.com>
2025-07-22 16:12:21 +0000

    add storybook/preview-api for field reactivity

--- src/components/atoms/input/Input.stories.tsx
+++ src/components/atoms/input/Input.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { Input } from "./Input";
+import { useArgs } from "@storybook/preview-api";
 import { InputProps } from "./Input.types";
 
 const meta: Meta<InputProps> = {
@@ -11,45 +12,59 @@ const meta: Meta<InputProps> = {
 export default meta
 type Story = StoryObj<InputProps>
 
+const Template = (args: InputProps): React.JSX.Element => {
+  const [, updateArgs] = useArgs();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    updateArgs({ value: e.target.value });
+    args.onChange?.(e);
+  };
+
+  return <Input {...args} onChange={handleChange} />;
+};
+
 export const Default: Story = {
+  render: Template,
   args: {
-    value: "",
-    onChange: () => {},
     placholder: "Placeholder",
     type: "text",
     helperText: "Helper text",
-    errorText: "Error text",
+    errorText: "",
     withIcon: false,
     icon: null,
-    disabled: false
+    disabled: false,
   },
 };
 
 export const WithHelperText: Story = {
+  render: Template,
   args: {
     ...Default.args,
     helperText: "Helper text",
-  }
-}
+  },
+};
 
 export const WithErrorText: Story = {
+  render: Template,
   args: {
     ...Default.args,
     errorText: "Error text",
-  }
-}
+  },
+};
 
 export const WithIcon: Story = {
+  render: Template,
   args: {
     ...Default.args,
     withIcon: true,
-    icon: <span>🫠</span>
-  }
-}
+    icon: <span>🫠</span>,
+  },
+};
 
 export const Disabled: Story = {
+  render: Template,
   args: {
     ...Default.args,
     disabled: true,
-  }
-}
+  },
+};
